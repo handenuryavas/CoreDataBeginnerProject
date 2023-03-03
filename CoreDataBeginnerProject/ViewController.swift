55acb1e91af0bd760932f2f1b198cd17071012e0
handenuryavas <hande.nur038@gmail.com>
2023-03-03 13:08:57 +0300

    Initial Commit

---
+++ CoreDataBeginnerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataBeginnerProject
+//
+//  Created by Hande Nur Yavaş on 3.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
